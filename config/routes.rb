0928a6f7c1aae8b1314d49a268a08053efdc8164
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-06 21:54:03 -0500

    Add: routes for shifts, company and schedule endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
+      resources :contracts
+      resources :companies
+      resources :schedules
+      resources :shifts
       
       post '/signin', to: 'auth#signin'
       post '/signup', to: 'auth#signup'
