2d8407f193be19768880f5f5ac12dd9322cd7f31
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-18 18:59:57 -0500

    Fix: initial data generation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
 [
   {
-    email: "user1@email.com",
-    first_name: "user 1",
+    email: "paco@email.com",
+    first_name: "Paco",
     last_name: "doe",
     password: "12345678",
     color: "#f1f1f1"
   },
   {
-    email: "user2@email.com",
-    first_name: "user 2",
+    email: "hugo@email.com",
+    first_name: "Hugo",
     last_name: "doe",
     password: "12345678",
     color: "#f1f134"
   },
   {
-    email: "user3@email.com",
-    first_name: "user 3",
+    email: "luis@email.com",
+    first_name: "Luis",
     last_name: "doe",
     password: "12345678",
     role: "admin",
@@ -52,35 +53,43 @@ Contract.all.each do |contract|
   Schedule.create(start_hour: '10:00', end_hour: '23:00', duration: 60, weekend: true, contract_id: contract.id)
 end
 
-User.all.each do |user|
-  Company.all.each do |company|
-    puts "#{company.name}"
-    company.contracts.each do |contract|
-      contract.schedules.each do |schedule|
-        if schedule.weekend
-          days_for_saturday = (6 - (Time.now.wday + 7)) % 7
-          start = Time.now.advance(days: days_for_saturday)
-          2.times do |i|
-            start = start.change(hour: schedule.start_hour.hour)
-            
-            while(start.hour < schedule.end_hour.hour)
-              Shift.create(start_hour: start, end_hour: start.advance(minutes: schedule.duration), week: start.strftime('%U'), user_id: user.id, schedule_id: schedule.id, company_id: company.id)
-              start = Shift.last.end_hour
-            end
-            start = start.advance(days: 1)
+Company.all.each do |company|
+  puts company.name
+  User.all.each do |user|
+    puts user.first_name
+    Support.create(company_id: company.id, user_id: user.id)
+  end
+end
+
+Company.all.each do |company|
+  puts company.name
+  company.contracts.each do |contract|
+    puts "contract #{contract.id}"
+    contract.schedules.order(weekend: :asc).each do |schedule|
+      puts "schedule #{schedule.id}"
+      week = Time.now.utc.strftime('%U')
+      if schedule.weekend
+        days_for_saturday = (6 - (Time.now.utc.wday + 7)) % 7
+        start = Time.now.utc.advance(days: days_for_saturday, weeks: 1)
+        2.times do |i|
+          start = start.change(hour: schedule.start_hour.hour)
+          while(start.hour < schedule.end_hour.hour)
+            Shift.create(start_hour: start, end_hour: start.advance(minutes: schedule.duration), week: week, schedule_id: schedule.id, company_id: company.id)
+            start = start.advance(minutes: schedule.duration)
           end
-        else
-          days_for_monday = (1 - (Time.now.wday + 7)) % 7
-          start = Time.now.advance(days: days_for_monday)
-          5.times do |i|
-            start = start.change(hour: schedule.start_hour.hour)
-            
-            while(start.hour < schedule.end_hour.hour)
-              Shift.create(start_hour: start, end_hour: start.advance(minutes: schedule.duration), week: start.strftime('%U'), user_id: user.id, schedule_id: schedule.id, company_id: company.id)
-              start = Shift.last.end_hour
-            end
-            start = start.advance(days: 1)
+          start = start.advance(days: 1)
+        end
+      else
+        days_for_monday = (1 - (Time.now.utc.wday + 7)) % 7
+        start = Time.now.utc.advance(days: days_for_monday)
+        5.times do |i|
+          start = start.change(hour: schedule.start_hour.hour)
+          
+          while(start.hour < schedule.end_hour.hour)
+            Shift.create(start_hour: start, end_hour: start.advance(minutes: schedule.duration), week: week, schedule_id: schedule.id, company_id: company.id)
+            start = start.advance(minutes: schedule.duration)
           end
+          start = start.advance(days: 1)
         end
       end
     end
