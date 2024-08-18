e23a852a0db6af3705d17ef1f2a8bf3a73c210eb
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-18 18:59:10 -0500

    Add: new routes and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
       resources :companies
       resources :schedules
       resources :shifts
+      resources :applications, only: %i[create]
       
       post '/signin', to: 'auth#signin'
       post '/signup', to: 'auth#signup'
-      get '/confirmed', to: 'shifts#confirmed'
+      get '/check_status', to: 'auth#check_status'
+      get '/confirmed/:id', to: 'shifts#confirmed'
       get '/weeks', to: 'shifts#weeks'
+      get '/verify_assignment/:id', to: 'shifts#verify_assignment'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_07_033908) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_18_202134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.boolean "is_confirmed"
+    t.bigint "user_id", null: false
+    t.bigint "shift_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shift_id"], name: "index_applications_on_shift_id"
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -43,19 +53,24 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_07_033908) do
   end
 
   create_table "shifts", force: :cascade do |t|
-    t.datetime "start_hour", null: false
-    t.datetime "end_hour", null: false
-    t.boolean "is_confirmed", default: false
-    t.bigint "user_id", null: false
+    t.datetime "start_hour"
+    t.datetime "end_hour"
+    t.string "week"
     t.bigint "schedule_id", null: false
     t.bigint "company_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "week"
-    t.boolean "is_postulated", default: false
     t.index ["company_id"], name: "index_shifts_on_company_id"
     t.index ["schedule_id"], name: "index_shifts_on_schedule_id"
-    t.index ["user_id"], name: "index_shifts_on_user_id"
+  end
+
+  create_table "supports", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_supports_on_company_id"
+    t.index ["user_id"], name: "index_supports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,9 +84,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_07_033908) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "applications", "users"
   add_foreign_key "contracts", "companies"
   add_foreign_key "schedules", "contracts"
   add_foreign_key "shifts", "companies"
   add_foreign_key "shifts", "schedules"
-  add_foreign_key "shifts", "users"
+  add_foreign_key "supports", "companies"
+  add_foreign_key "supports", "users"
 end
