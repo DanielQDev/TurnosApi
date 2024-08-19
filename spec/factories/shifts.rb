44dc6d969749ecc76df0e959bcccdd6cdd3bdef3
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-18 19:00:51 -0500

    Opt: data testing

--- spec/factories/shifts.rb
+++ spec/factories/shifts.rb
@@ -1,11 +1,8 @@
 FactoryBot.define do
   factory :shift do
-    start_hour { "2024-08-06 17:00:00" }
-    end_hour { "2024-08-06 18:00:00" }
-    is_confirmed { false }
-    is_postulated { false }
-    week { 31 }
-    user
+    start_hour { "2024-08-14 17:00:00" }
+    end_hour { "2024-08-14 18:00:00" }
+    week { "32" }
     schedule
     company
   end
